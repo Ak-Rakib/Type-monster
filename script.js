810f840fb6d452e183c8a8463beb6c829b5b2d18
AK-Rakib <rkrakibsaimon@gmail.com>
2022-09-15 01:35:22 +0600

    2nd bug fixed

--- script.js
+++ script.js
@@ -108,19 +108,23 @@ const start = () => {
 
   const startCountdown = setInterval(() => {
     countdownOverlay.innerHTML = `<h1>${count}</h1>`;
+    
 
     // finished timer
     if (count == 0) {
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
       countdownOverlay.style.display = "flex";
+      countdownOverlay.style.display = "none";
       display.classList.remove("inactive");
+      
 
       clearInterval(startCountdown);
       startTime = new Date().getTime();
     }
     count--;
   }, 1000);
+  
 };
 
 // START Countdown
@@ -135,5 +139,5 @@ setInterval(() => {
   const timeSpent = (currentTime - startTime) / 1000;
 
 
-  document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
+  document.getElementById("show-time").innerHTML = `${Math.floor(startTime ? timeSpent : 0)} seconds`;
 }, 1000);
